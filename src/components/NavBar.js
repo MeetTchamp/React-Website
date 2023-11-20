f39cfc8d4445b266d1d528679f170b78e3e5a4dd
meet dwivedi <meet.v.dwivedi2808@gmail.com>
2023-11-20 13:07:16 +0530

    basic style navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 const NavBar = () => {
-  
   return (
     <div id="nav">
-      <div>
-        <h1>Little Champ</h1>
+      <div className="nav-container">
+        <h1 id="little-champ">Little Champ</h1>
+        <ul className="ul">
+          <li className="li">Home</li>
+          <li className="li">About us</li>
+          <li className="li">Contact us</li>
+        </ul>
       </div>
     </div>
   );
